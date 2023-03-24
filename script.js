f96a30a9444ae5401a4ff7bc948c896de01124cc
Jpc77 <jpcardenasgonzalez@gmail.com>
2023-03-24 14:21:31 -0500

    Agregar .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,19 @@
+//const p = document.querySelector('p');
+//const parrafito = document.querySelector('.parrafito');
+//const parrafito = document.getElementsByClassName('parrafito');
+//const pid = document.querySelector('#pid');
+//const pid = document.getElementById('pid');
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+form.addEventListener('submit', sumarInputValues);
+
+function sumarInputValues(event) {
+    event.preventDefault();
+    const sumaInputs =  input1.value + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
